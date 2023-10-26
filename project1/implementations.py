730b073a2d4e251455c59ab4652ce97474761cf7
Ivonne320 <ivonne.don.epfl@gmail.com>
2023-10-26 23:19:43 +0200

    add NN

--- project1/implementations.py
+++ project1/implementations.py
@@ -574,9 +574,9 @@ def process_y(ty):
 
 # calculate accuracy
 def predict_acc_pure(y_pred, y_val):
-    accuracy = (y_pred == y_val).sum() / len(y_val)
+    accuracy = (y_pred == y_val)[0].sum() / len(y_val)
     print("The Accuracy is: %.4f"%accuracy)
-    return acc
+    return accuracy
 
 # calculate F1 score
 def predict_f1_pure(y_pred, y_val):
@@ -799,4 +799,267 @@ def polynomial_expansion_single(x, degree):
     return np.hstack(expanded_features)
 
 
+
+########## Neural Network ##########
+
+
+# class NeuralNetwork:
+#     def __init__(self, input_size, hidden_size, output_size):
+#         # Initialize weights and biases
+#         self.W1 = np.random.randn(input_size, hidden_size)
+#         self.b1 = np.zeros((1, hidden_size))
+#         self.W2 = np.random.randn(hidden_size, output_size)
+#         self.b2 = np.zeros((1, output_size))
+    
+#     def sigmoid(self, z):
+#         return 1 / (1 + np.exp(-z))
+    
+#     def sigmoid_derivative(self, z):
+#         return self.sigmoid(z) * (1 - self.sigmoid(z))
+    
+#     def forward(self, X):
+#         # Hidden layer
+#         self.z1 = np.dot(X, self.W1) + self.b1
+#         self.a1 = self.sigmoid(self.z1)
+#         # Output layer
+#         self.z2 = np.dot(self.a1, self.W2) + self.b2
+#         self.a2 = self.sigmoid(self.z2)
+#         return self.a2
+    
+#     def backward(self, X, y, learning_rate=0.01):
+#         m = X.shape[0]
+        
+#         # Calculate the output layer error
+#         dz2 = self.a2 - y
+#         dW2 = 1/m * np.dot(self.a1.T, dz2)
+#         db2 = 1/m * np.sum(dz2, axis=0, keepdims=True)
+        
+#         # Calculate the hidden layer error
+#         dz1 = np.dot(dz2, self.W2.T) * self.sigmoid_derivative(self.z1)
+#         dW1 = 1/m * np.dot(X.T, dz1)
+#         db1 = 1/m * np.sum(dz1, axis=0, keepdims=True)
+        
+#         # Update the weights and biases
+#         self.W2 -= learning_rate * dW2
+#         self.b2 -= learning_rate * db2
+#         self.W1 -= learning_rate * dW1
+#         self.b1 -= learning_rate * db1
+        
+#     def train(self, X, y, epochs=1000, learning_rate=0.01):
+#         for epoch in range(epochs):
+#             # Forward pass
+#             predictions = self.forward(X)
+#             # Compute loss
+#             loss = -np.mean(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+#             # Backward pass
+#             self.backward(X, y, learning_rate)
+#             if epoch % 100 == 0:
+#                 print(f"Epoch: {epoch}, Loss: {loss:.4f}")
+#         return self
+
+#     def predict(self, X):
+#         predictions = self.forward(X)
+#         return (predictions > 0.5).astype(int)
+
+# # Example usage
+# if __name__ == "__main__":
+#     np.random.seed(42)
+    
+#     # Generate some random data for training
+#     X = np.random.randn(1000, 2)
+#     y = (X[:, 0] + X[:, 1] > 0).astype(int).reshape(-1, 1) # Simple linear boundary
+
+#     nn = NeuralNetwork(input_size=2, hidden_size=5, output_size=1)
+#     nn.train(X, y, epochs=1000, learning_rate=0.1)
+    
+#     # Test predictions
+#     test_X = np.array([[0.5, 0.5], [-0.5, -0.5]])
+#     predictions = nn.predict(test_X)
+#     print("Test Predictions:", predictions)
+
+
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
+
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, x * alpha)
+
+def leaky_relu_derivative(x, alpha=0.01):
+    return np.where(x > 0, 1, alpha)
+
+# def initialize_network(input_size, hidden_size, output_size):
+def initialize_network(input_size, hidden_size1, hidden_size2, hidden_size3, output_size):
+    network = []
+    # hidden_layer = {'W': np.random.randn(input_size, hidden_size)*0.1,
+    #                 'b': np.zeros((1, hidden_size))}
+    # output_layer = {'W': np.random.randn(hidden_size, output_size)*0.1,
+    #                 'b': np.zeros((1, output_size))}
+    # network.append(hidden_layer)
+    # network.append(output_layer)
+
+    hidden_layer1 = {'W': np.random.randn(input_size, hidden_size1)*np.sqrt(2./input_size),
+                    'b': np.zeros((1, hidden_size1))}
+    hidden_layer2 = {'W': np.random.randn(hidden_size1, hidden_size2)*np.sqrt(2./hidden_size1),
+                    'b': np.zeros((1, hidden_size2))}
+    hidden_layer3 = {'W': np.random.randn(hidden_size2, hidden_size3)*np.sqrt(2./hidden_size2),
+                    'b': np.zeros((1, hidden_size3))}
+    output_layer = {'W': np.random.randn(hidden_size3, output_size)*np.sqrt(2./hidden_size3),
+                    'b': np.zeros((1, output_size))}
+    network.append(hidden_layer1)
+    network.append(hidden_layer2)
+    network.append(hidden_layer3)
+    network.append(output_layer)
+
+    return network
+
+def forward_propagation(network, x):
+    z1 = np.dot(x, network[0]['W']) + network[0]['b']
+    a1 = leaky_relu(z1)
+    # z2 = np.dot(a1, network[1]['W']) + network[1]['b']
+    # a2 = sigmoid(z2)
+    z2 = np.dot(a1, network[1]['W']) + network[1]['b'] 
+    a2 = leaky_relu(z2)
+
+    z3 = np.dot(a2, network[2]['W']) + network[2]['b']
+    a3 = leaky_relu(z3)
+
+
+    z4 = np.dot(a3, network[3]['W']) + network[3]['b']
+    a4 = sigmoid(z4)
+
+    # clip a2 values
+    epsilon = 1e-7
+    # a2 = np.clip(a2, epsilon, 1 - epsilon)
+    # activations = [a1, a2]
+    a4 = np.clip(a4, epsilon, 1 - epsilon)
+
+    # clip z4 values
+    # z4 = np.clip(z4, epsilon, 1 - epsilon)
+    activations = [a1, a2, a3, a4]
+    return activations
+
+def backpropagation(network, x, y, activations):
+    m = x.shape[0]
+    # a1, a2 = activations
+    a1, a2, a3, a4 = activations
+    
+    # Start from the output layer
+    # dz2 = a2 - y.reshape(-1, 1)
+    # dw2 = np.dot(a1.T, dz2) / m
+    # db2 = np.sum(dz2, axis=0, keepdims=True) / m
+
+    # da1 = np.dot(dz2, network[1]['W'].T)
+    # dz1 = da1 * relu_derivative(a1)
+    # dw1 = np.dot(x.T, dz1) / m
+    # db1 = np.sum(dz1, axis=0, keepdims=True) / m
+
+   
+    # loss = -np.mean(y * np.log(z4) + (1 - y) * np.log(1 - z4))
+
+    dz4 = a4 - y.reshape(-1, 1)
+    dw4 = np.dot(a3.T, dz4) / m
+    db4 = np.sum(dz4, axis=0, keepdims=True) / m
+
+    # dz3 = a3 - y.reshape(-1, 1)
+    # dw3 = np.dot(a2.T, dz3) / m
+    # db3 = np.sum(dz3, axis=0, keepdims=True) / m
+
+    da3 = np.dot(dz4, network[3]['W'].T)
+    dz3 = da3 * leaky_relu_derivative(a3)
+    dw3 = np.dot(a2.T, dz3) / m
+    db3 = np.sum(dz3, axis=0, keepdims=True) / m
+
+    da2 = np.dot(dz3, network[2]['W'].T)
+    dz2 = da2 * leaky_relu_derivative(a2)
+    dw2 = np.dot(a1.T, dz2) / m
+    db2 = np.sum(dz2, axis=0, keepdims=True) / m
+
+    da1 = np.dot(dz2, network[1]['W'].T)
+    dz1 = da1 * leaky_relu_derivative(a1)
+    dw1 = np.dot(x.T, dz1) / m
+    db1 = np.sum(dz1, axis=0, keepdims=True) / m
+
+    max_int = 1
+    dw1 = np.clip(dw1, -max_int, max_int)
+    dw2 = np.clip(dw2, -max_int, max_int)
+    dw3 = np.clip(dw3, -max_int, max_int)
+    dw4 = np.clip(dw4, -max_int, max_int)
+    db1 = np.clip(db1, -max_int, max_int)
+    db2 = np.clip(db2, -max_int, max_int)
+    db3 = np.clip(db3, -max_int, max_int)
+    db4 = np.clip(db4, -max_int, max_int)
+    
+
+    # return [{"dW": dw1, "db": db1}, {"dW": dw2, "db": db2}]
+    return [{"dW": dw1, "db": db1}, {"dW": dw2, "db": db2}, {"dW": dw3, "db": db3}, {"dW": dw4, "db": db4}]
+
+def update_weights(network, gradients, learning_rate):
+    for layer, gradient in zip(network, gradients):
+        layer['W'] -= learning_rate * gradient['dW']
+        layer['b'] -= learning_rate * gradient['db']
+
+def train(network, X, y, learning_rate, epochs, batch_size):
+    m = X.shape[0]
+    for epoch in range(epochs):
+        indices = np.arange(m)
+        np.random.shuffle(indices)
+        X_shuffled = X[indices]
+        y_shuffled = y[indices]
+        
+        for i in range(0, m, batch_size):
+            X_batch = X_shuffled[i:i + batch_size]
+            y_batch = y_shuffled[i:i + batch_size]
+            
+            activations = forward_propagation(network, X_batch)
+            # a2 = activations[-1]
+            # loss = -np.mean(y_batch * np.log(a2) + (1 - y_batch) * np.log(1 - a2))
+            a4 = activations[-1]
+            # loss = -np.mean(y_batch * np.log(a4) + (1 - y_batch) * np.log(1 - a4))
+            logprobs = np.dot(y_batch.reshape(1,-1),np.log(a4))+np.dot((1-y_batch.reshape(1,-1)),np.log(1-a4)) 
+            loss = -logprobs / m
+        
+            gradients = backpropagation(network, X_batch, y_batch, activations)
+            update_weights(network, gradients, learning_rate)
+        
+        if epoch % 2 == 0:  # print loss every 100 epochs
+            print("Epoch:", epoch, "Loss:", loss)
+    
+    return network
+
+def predict(network, X):
+    
+    # Get the final activations from forward propagation
+    # a2 = forward_propagation(network, X)[-1]
+    a4 = forward_propagation(network, X)[-1]
+    
+    # Convert activations to binary predictions (0 or 1)
+    # predictions = (np.squeeze(a2) > 0.5).astype(int)
+    predictions = (np.squeeze(a4) > 0.5).astype(int)
+    
+    return predictions
+
+
+
+##### Nerual Network with keras  #####
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.preprocessing import LabelEncoder
+# from keras.wrappers.scikit_learn import KerasClassifier
+# from keras.utils import np_utils
+
+# # for modeling
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout
+# from keras.callbacks import EarlyStoppingpip
 
